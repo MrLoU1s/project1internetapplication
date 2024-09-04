a9fb640763076c34e29821894057d180edebb7eb
MrLoU1s <louis.karanja@strathmore.edu>
2024-09-04 14:54:12 +0300

    showing file name

--- load.php
+++ load.php
@@ -1,8 +1,23 @@
 <?php
 
-require_once "fnc.php";
+require_once "user_details.php";
 
 $obj = new user_details();
+
+
+$arr = ["black","white","green","yellow"];
+
+foreach($arr AS $color){
+    print $color. "<br>";
+}
+
+//brings path of current directory!
+print dirname(__FILE__);
+print "<br>";
+print "<br>";
+
+//shows the current file name
+print basename($_SERVER["PHPh_SELF"]);
 
 
 ?>
